de1137648a4c9ec45b27af1b32fe8ae71c85bb7f
culley-aidan <culley-aidan@users.noreply.github.com>
2019-11-04 10:28:09 -0600

    finished

---
+++ SumInts/SumInts/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int finalAddition = 0;
+            bool loop = true;
+            while (loop)
+            {
+                Console.Write("Enter a number: ");
+                int.TryParse(Console.ReadLine(), out int numberEntered);
+                if (numberEntered != 999) {
+                    finalAddition += numberEntered;
+                } else {
+                    loop = false;
+                }
+            }
+            Console.WriteLine("your final number is: {0}", finalAddition);
+        }
+    }
+}
