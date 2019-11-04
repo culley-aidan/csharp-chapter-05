3cd4eb6d07d86d1ceea4f5a63b01aeea186646ad
culley-aidan <culley-aidan@users.noreply.github.com>
2019-11-04 10:37:39 -0600

    started

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to display multiplication table for: ");
+            int.TryParse(Console.ReadLine(), out int number);
+        }
+    }
+}
