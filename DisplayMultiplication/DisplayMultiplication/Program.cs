017f2516145d2c0e6e4ff38edc1ffac0fd47f473
culley-aidan <culley-aidan@users.noreply.github.com>
2019-11-06 10:05:39 -0600

    finished

---
+++ DisplayMultiplication/DisplayMultiplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DisplayMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write(i + "\t");
+                for (int j = 1; j <= 2; j++)
+                {
+                    Console.Write(i == 0 ? (j + "\t") : (i * j + "\t"));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ C# Programming Chapter 5
 ## TODO
 - [X] 02 - SumInts
 - [ ] 05 - HomeSales
-- [ ] 06 – DisplayMultiplication
+- [X] 06 – DisplayMultiplication
 - [X] 08 – MultiplicationTable
 - [ ] 11 - TippingTable3
 - [ ] 15 - GuessingGame3
